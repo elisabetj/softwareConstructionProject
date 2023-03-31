b7e9e7ded35605574bed283ec1ecbe111ac38975
EsterJenny <ejb7@hi.is>
2023-03-31 09:49:26 +0000

    trips.java

---
+++ src/main/java/is/hi/hbv202g/Trips.java
@@ -0,0 +1,12 @@
+package is.hi.hbv202g;
+
+/**
+ * @author Ester Jenný (ejb7)
+ **/
+public class Trips {
+
+    private String[] tripsAvailable(){
+        String [] trips = {"Horseriding", "Snowboarding"};
+        return trips;
+    }
+}
