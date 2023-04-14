c0ebb1be06f7b4a4f1e9783b7b49a42e1686d772
EsterJenny <ejb7@hi.is>
2023-04-14 09:52:51 +0000

    Bakendi

---
+++ src/main/java/is/hi/hbv202g/Customer.java
@@ -0,0 +1,30 @@
+package is.hi.hbv202g;
+
+/**
+ * @author Ester Jenný (ejb7)
+ **/
+public class Customer {
+    private String name;
+    private String email;
+
+    public Customer(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
