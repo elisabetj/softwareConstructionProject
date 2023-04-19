da7fdc8697a3a013655564354e791fd758947144
elisabetj <116761221+elisabetj@users.noreply.github.com>
2023-04-19 16:48:11 +0000

    lagalaga

--- src/main/java/is/hi/hbv202g/Controller.java
+++ src/main/java/is/hi/hbv202g/Controller.java
@@ -1,5 +1,46 @@
 package is.hi.hbv202g;
 
+import javafx.fxml.FXML;
+
+import javax.swing.text.html.ListView;
+import java.awt.*;
+import java.util.ResourceBundle;
+
 public class Controller {
+
+    // name
+    @FXML
+    private TextField ;
+
+    // email
+    @FXML
+    private TextField ;
+
+    // reservation
+    @FXML
+    private ListView hotelList;
+
+
+    // hotel eda trip
+
+    @FXML
+    private ListView tourList;
+
+    private Data data = new Data();
+    // private ObserbleList a eftir ad setja inn Customer
+
+    @Override
+    public void initialse(URL locaton, ResourceBundle resources) {
+        customer = data.getCustomer();
+
+    }
+
+
+
+
+
+
+
+
 
 }
